0a13d1da0bd4c8cac48524bfa57e25d53f6a40b7
Jesse <1016739@student.sae.edu.au>
2019-10-22 11:34:23 +1100

    Shooting Stuff

---
+++ Assets/Testing/Scripts/bullet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    public float bulletSpeed;
+    public float despawnTime;
+    // Update is called once per frame
+    void Update()
+    {
+        Rigidbody bulletRB = this.GetComponent<Rigidbody>();
+        bulletRB.velocity = transform.forward * bulletSpeed;
+    }
+
+    private void OnCollisionEnter(Collision ballCol)
+    {
+        if (ballCol.other.tag == "Wall")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Testing/Scripts/bulletSpawner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletSpawner : MonoBehaviour
+{
+    public GameObject bullet;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire3"))
+        {
+            Instantiate(bullet, transform.position, transform.rotation);
+        }
+    }
+}
