025c8655287fd028b2c789124104030ac2dc028c
Rohith Maddali <1015083@student.sae.edu.au>
2019-10-26 04:29:18 +1100

    Added pick up script for weapons and ammo

--- Assets/Scripts/AWSpawner.cs
+++ Assets/Scripts/AWSpawner.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class AWSpawner : MonoBehaviour
 {
+    
+    public GameObject ammo;
+    public List<GameObject> weapon;
+
+    public bool picked = false;
+    public int timer;
+    public int count;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject A = Instantiate(ammo, transform.position, transform.rotation);
+        A.GetComponent<PickUpA>().awSpawner = this;
+        count++;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        StartCoroutine(waitTime());
+    }
+
+    IEnumerator waitTime()
+    {
+        if(picked == true)
+        {
+            picked = false;
+            yield return new WaitForSeconds(timer);
+            if (count == 5 && picked == true)
+            {
+                int W = Random.Range(0, weapon.Count);
+                GameObject A = Instantiate(weapon[W], transform.position, transform.rotation);
+                A.GetComponent<PickUpA>().awSpawner = this;
+                //resets the countdown for weapon spawn.
+                count = 0;
+            }
+            else if (count < 5 && picked == true)
+            {
+                GameObject A = Instantiate(ammo, transform.position, transform.rotation);
+                A.GetComponent<PickUpA>().awSpawner = this;
+                count++;
+            }
+        }
     }
 }
--- Assets/Scripts/PickUpA.cs
+++ Assets/Scripts/PickUpA.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class PickUpA : MonoBehaviour
 {
-    public GameObject weapon;
-    public GameObject ammo;
+    public AWSpawner awSpawner;
+    public bool picked = false;
+
+
+    public int ammoCount;
 
     void OnTriggerEnter(Collider col)
     {
@@ -13,8 +16,28 @@ public class PickUpA : MonoBehaviour
         //if the player has less ammo, the player will be able to pick it up.
         if (col.gameObject.tag == "Player 1" || col.gameObject.tag == "Player 2")
         {
-
+            GameObject player = col.gameObject;
+            //checks if player is eligible to pick up the ammo block.
+            if(player.GetComponent<playerStats>().curAmmo < player.GetComponent<playerStats>().maxAmmo)
+            {
+                player.GetComponent<playerStats>().curAmmo += ammoCount;
+                //checks if the player is picking up more than the max ammo that he can store in that weapon.
+                if(player.GetComponent<playerStats>().curAmmo > player.GetComponent<playerStats>().maxAmmo)
+                player.GetComponent<playerStats>().curAmmo = player.GetComponent<playerStats>().maxAmmo;
+                des();
+                awSpawner.picked = true;
+            }
+            else
+            {
+                Debug.Log("Ammo is full");
+            }
         }
     }
-            
+
+
+    void des()
+    {
+        //destroys the game object once its picked up.
+        Destroy(gameObject);
+    }
 }
