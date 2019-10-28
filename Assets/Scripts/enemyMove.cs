c3937259f67d3f0bfffe8d74ca567db9c48a0d53
Jesse <1016739@student.sae.edu.au>
2019-10-28 15:17:50 +1100

    Enemy Movement Code
    7 Lines of Life Creation Boys

---
+++ Assets/Scripts/enemyMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class enemyMove : MonoBehaviour
+{
+    public GameObject trackPoint;
+    public Vector3 pointPos;
+
+    private NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pointPos = trackPoint.transform.position;
+        agent.destination = pointPos;
+    }
+}
