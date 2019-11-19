f0c0760262bc5d145abc7be29fe505991de811af
Rohith <1015083@student.sae.edu.au>
2019-11-19 09:31:14 +0800

    Added pseudo code

---
+++ Assets/Scripts/BattleManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Player takes damage from enemy attacks
+    //player health decreases
+    //player attacks
+    //player ammo decreases if its a picked up gun
+
+    //enemy takes damage from player atatcks
+    //enemy health decreases
+    //enemy attacks
+
+    //wave counter
+    //despawn current wave
+    //spawn next wave after a few seconds
+    //increase wave counter
+    //check wave count to trigger boss level
+    //transition to boss level
+}
